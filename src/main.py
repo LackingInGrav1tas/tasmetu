d775dbd5952cc2605c68c672d19f1166fe760f18
a <42680395+LackingInGrav1tas@users.noreply.github.com>
2022-12-23 17:15:22 -0500

    with recording + elapsed time
    waits before stopping recording

--- src/main.py
+++ src/main.py
@@ -6,17 +6,42 @@ CHUNK = 1024
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 44100
-THRESHOLD = 1000
+THRESHOLD = 200
 
 p = pyaudio.PyAudio()
 
 stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
 
+# output file
+wf = wave.open('output.wav', 'wb')
+wf.setnchannels(CHANNELS)
+wf.setsampwidth(p.get_sample_size(FORMAT))
+wf.setframerate(RATE)
+
 while True:
+    print("waiting")
     data = stream.read(CHUNK)
     audio_data = np.frombuffer(data, dtype=np.int16)
     volume = np.abs(audio_data).mean()
-    print(volume)
+    
+    if volume >= THRESHOLD:
+
+        elapsed_silence = 0
+
+        while volume >= THRESHOLD or elapsed_silence <= 150: # elapsed silence allows for about 3 seconds of silence before stopping recording
+            print("recording")
+            data = stream.read(CHUNK)
+            audio_data = np.frombuffer(data, dtype=np.int16)
+            volume = np.abs(audio_data).mean()
+
+            if volume < THRESHOLD: elapsed_silence += 1
+            else: elapsed_silence = 0
+
+            wf.writeframes(data)
+
+        wf.close()
+        break
+    
 
 
 stream.stop_stream()
