19628c780babe1bb9af0673fe529a68016f51ec5
a <42680395+LackingInGrav1tas@users.noreply.github.com>
2022-12-24 15:01:33 -0500

    error handling

--- src/main.py
+++ src/main.py
@@ -15,44 +15,49 @@ p = pyaudio.PyAudio()
 stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
 
 while True:
-    print("waiting")
-    data = stream.read(CHUNK)
-    audio_data = np.frombuffer(data, dtype=np.int16)
-    volume = np.abs(audio_data).mean()
-    
-    if volume >= THRESHOLD:
+    try:
+        print("waiting")
+        data = stream.read(CHUNK)
+        audio_data = np.frombuffer(data, dtype=np.int16)
+        volume = np.abs(audio_data).mean()
+        
+        if volume >= THRESHOLD:
 
-        # output file
-        name = str(datetime.now()).replace(':', '.')
-        wf = wave.open(name + '.wav', 'wb')
-        wf.setnchannels(CHANNELS)
-        wf.setsampwidth(p.get_sample_size(FORMAT))
-        wf.setframerate(RATE)
+            # output file
+            name = str(datetime.now()).replace(':', '.')
+            wf = wave.open(name + '.wav', 'wb')
+            wf.setnchannels(CHANNELS)
+            wf.setsampwidth(p.get_sample_size(FORMAT))
+            wf.setframerate(RATE)
 
-        elapsed_silence = 0
+            elapsed_silence = 0
 
-        while volume >= THRESHOLD or elapsed_silence <= 150: # elapsed silence allows for about 3 seconds of silence before stopping recording
-            print("recording")
-            data = stream.read(CHUNK)
-            audio_data = np.frombuffer(data, dtype=np.int16)
-            volume = np.abs(audio_data).mean()
+            while volume >= THRESHOLD or elapsed_silence <= 150: # elapsed silence allows for about 3 seconds of silence before stopping recording
+                print("recording")
+                data = stream.read(CHUNK)
+                audio_data = np.frombuffer(data, dtype=np.int16)
+                volume = np.abs(audio_data).mean()
 
-            if volume < THRESHOLD: elapsed_silence += 1
-            else: elapsed_silence = 0
+                if volume < THRESHOLD: elapsed_silence += 1
+                else: elapsed_silence = 0
 
-            wf.writeframes(data)
+                wf.writeframes(data)
 
-        wf.close()
+            wf.close()
 
-        # transcription
-        r = sr.Recognizer()
-        with sr.AudioFile(name + '.wav') as source:
-            audio = r.record(source)
-            f = open(name + '.txt', 'a')
-            f.write(str(r.recognize_google(audio)))
-            f.close()
+            # transcription
+            r = sr.Recognizer()
+            with sr.AudioFile(name + '.wav') as source:
+                audio = r.record(source)
+                f = open(name + '.txt', 'a')
+                transcription = str(r.recognize_google(audio))
+                f.write(transcription)
+                f.close()
 
-        break
+            # break
+    except Exception as e:
+        f = open('error.txt', 'a')
+        f.write(str(e))
     
 
 
