17afb96547c40a72ab4f3490252d0f608723344a
a <42680395+LackingInGrav1tas@users.noreply.github.com>
2022-12-29 16:11:06 -0500

    restructuring

---
+++ capturer/audio_scanner.py
@@ -0,0 +1,87 @@
+import pyaudio, wave, multiprocessing
+import numpy as np
+from matplotlib import pyplot as plt
+
+p = pyaudio.PyAudio()
+
+class AudioScanner:
+    def __init__(self, chunk, format, channels, rate, threshold, silence):
+        self.visualize = False
+        self.chunk = chunk
+        self.format = format
+        self.channels = channels
+        self.rate = rate
+        self.threshold = threshold
+        self.fn = None
+        self.silence = silence
+
+    def __del__(self):
+        if self.visualize: self.v.terminate()
+
+    def _visualization(self):
+        s = p.open(format=self.format, channels=self.channels, rate=self.rate, input=True, frames_per_buffer=self.chunk)
+
+        x = [ i for i in range(50)]
+        y = [ 0 for i in range(50) ]
+
+        plt.figure(num="Audio Stream")
+        plt.title("Audio Stream", color="white", weight=700)
+        plt.ylim([0, 3000])
+
+        while True:
+            data = s.read(self.chunk)
+            audio_data = np.frombuffer(data, dtype=np.int16)
+            volume = np.abs(audio_data).mean()
+
+            plt.clf()
+            y = y[1:]
+            y.append(volume)
+            plt.plot(x, y, label="stream")
+            plt.plot(x, [self.threshold for i in range(50)])
+            plt.ylabel("volume")
+            plt.pause(0.01)
+
+    def begin_visualization(self):
+        self.v = multiprocessing.Process(target=self._visualization, args=())
+        self.v.start()
+        self.visualize = True
+
+    def onReceive(self, function, name_function):
+        self.fn = function
+        self.name_fn = name_function       
+
+    def listen(self):
+        stream = p.open(format=self.format, channels=self.channels, rate=self.rate, input=True, frames_per_buffer=self.chunk)
+        
+        recording = False
+        elapsed_silence = 0
+        while True:
+            data = stream.read(self.chunk)
+            audio_data = np.frombuffer(data, dtype=np.int16)
+            volume = np.abs(audio_data).mean()
+            
+            if volume >= self.threshold:
+                elapsed_silence = 0
+
+                if not recording:
+                    # output file
+                    audio_file = self.name_fn()
+
+                    wf = wave.open(audio_file, 'wb')
+                    wf.setnchannels(self.channels)
+                    wf.setsampwidth(p.get_sample_size(self.format))
+                    wf.setframerate(self.rate)
+
+                recording = True
+
+            if recording:
+                wf.writeframes(data)
+
+                if volume < self.threshold:
+                    if elapsed_silence > 100:
+                        recording = False
+                        elapsed_silence = 0
+                        wf.close()
+                        if self.fn(audio_file): break
+
+                    else: elapsed_silence += 1
---
+++ capturer/main.py
@@ -0,0 +1,31 @@
+import speech_recognition as sr
+from audio_scanner import AudioScanner
+from datetime import datetime
+import pyaudio, os
+
+def transcribe(audio_file):
+    r = sr.Recognizer()
+    with sr.AudioFile(audio_file) as source:
+        audio = r.record(source)
+        f = open(audio_file.replace('wav', 'txt'), 'a')
+        try:
+            transcription = str(r.recognize_google(audio))
+        except:
+            print("unrecognized")
+            transcription = "unrecognized"
+        f.write(transcription)
+        f.close()
+
+    os.system('compact /c /q /i "file > NUL'.replace("file", audio_file))
+    if 'stop program' in transcription: return True
+    return False 
+
+def name():
+    return 'data/' + str(datetime.now()).replace(':', '.') + '.wav'
+
+if __name__ == "__main__":
+    asc = AudioScanner(1024, pyaudio.paInt16, 1, 44100, 300, 100)
+    asc.begin_visualization()
+    asc.onReceive(transcribe, name)
+    asc.listen()
+    del asc
