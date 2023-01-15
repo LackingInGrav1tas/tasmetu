79b30ea80094a1f72a1b859cea58457f54c8fa5e
a <42680395+LackingInGrav1tas@users.noreply.github.com>
2023-01-15 18:13:51 -0500

    date (day) format

--- scanner/main.py
+++ scanner/main.py
@@ -5,25 +5,40 @@ from transcription import transcribe
 
 def save(audio_file):
     os.system(f'sox "{audio_file}" -n noiseprof noise-profile.prof')
-    os.system(f'sox "{audio_file}" "{audio_file.replace("archive/", "archive/F-")}" noisered noise-profile.prof')
+    os.system(f'sox "{audio_file}" "{audio_file.replace("day)/", "day)/F-")}" noisered noise-profile.prof')
     os.remove(audio_file)
     os.remove("noise-profile.prof")
-    os.rename(audio_file.replace("archive/", "archive/F-"), audio_file)
+    os.rename(audio_file.replace("day)/", "day)/F-"), audio_file)
 
     f = open(audio_file.replace('wav', 'txt'), 'a')
     transcription = transcribe(audio_file)
     f.write(transcription)
     f.close()
 
-    os.system('compact /c /q /i "file > NUL'.replace("file", audio_file))
+    os.system('compact /c /q /i "file'.replace("file", audio_file))
     if 'stop program' in transcription: return True
-    return False 
+    return False
+
+days = {
+    0: 'Monday',
+    1: 'Tuesday',
+    2: 'Wednesday',
+    3: 'Thursday',
+    4: 'Friday',
+    5: 'Saturday',
+    6: 'Sunday'
+}
 
 def name():
-    return 'g:/My Drive/tasmetu-archive/' + str(datetime.now()).replace(':', '.') + '.wav'
+    dt = datetime.now()
+    path = f'g:/My Drive/tasmetu-archive/{str(dt.date())} ({days[dt.weekday()]})'
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+    return path + '/' + str(dt.time()).replace(':', '.') + '.wav'
 
 if __name__ == "__main__":
-    asc = AudioScanner(chunk=1024, format=pyaudio.paInt16, channels=1, rate=44100, threshold=300, silence=100)
+    asc = AudioScanner(chunk=1024, format=pyaudio.paInt16, channels=1, rate=44100, threshold=300, silence=150)
     asc.begin_visualization()
     asc.onReceive(save, name)
     asc.listen()
